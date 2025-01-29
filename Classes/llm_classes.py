06f685c2c7a77e8ba2c42651da1fa4f158e7a4dc
Wouter Born <wouter@born.gs>
2025-01-29 17:58:28 -0300

    chatgpt

--- Classes/llm_classes.py
+++ Classes/llm_classes.py
@@ -1,17 +1,26 @@
 from langchain_groq import ChatGroq
+from langchain_openai import ChatOpenAi
 ##################################
 # LLM Setup
 ##################################
-modelVers = ChatGroq(
-    temperature=0,
-    model_name="llama-3.3-70b-versatile",
-    api_key="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",
-    disable_streaming=True
-)
+# modelVers = ChatGroq(
+#     temperature=0,
+#     model_name="llama-3.3-70b-versatile",
+#     api_key="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",
+#     disable_streaming=True
+# )
 
-modelSpec = ChatGroq(
+# modelSpec = ChatGroq(
+#     temperature=0,
+#     model_name="llama-3.3-70b-specdec",
+#     api_key="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",
+#     disable_streaming=True
+# )
+
+modelVers = ChatOpenAi(
     temperature=0,
-    model_name="llama-3.3-70b-specdec",
-    api_key="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",
-    disable_streaming=True
-)
+    model_name="ChatGPT-4o")
+
+modelSpec = ChatOpenAi(
+    temperature=0,
+    model_name="ChatGPT-4o")
