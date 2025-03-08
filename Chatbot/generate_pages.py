bdcba08c7a7f88745062c3971a6890852bbbaa89
Matias <mpanario@gmail.com>
2025-03-08 13:58:41 -0300

    Updating generate pages code.

--- Chatbot/generate_pages.py
+++ Chatbot/generate_pages.py
@@ -1,8 +1,17 @@
+import json
+import logging
 from typing import List
 from langchain_core.messages import ( AIMessage, HumanMessage, SystemMessage, BaseMessage )
 from langchain_groq import ChatGroq
 from Nodes.load_xml_instructions import load_xml_instructions
 from langgraph.graph import MessagesState
+
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logger = logging.getLogger(__name__)
 
 
 # Initialize the model
@@ -13,11 +22,9 @@ model = ChatGroq(
     timeout=5,
     stop=None,
     model_kwargs={  # Explicitly pass additional parameters here
-        "model_provider":"groq",
-        "stream": False,
-        "response_format": {"type": "json_object"},
-        
-    }
+        "response_format": {"type": "json_object"},        
+    },
+    disable_streaming=True
 )
 
 def PagesClass(BaseModel):
@@ -28,11 +35,14 @@ def generate_pages(state: MessagesState):
     instructions = load_xml_instructions("Chatbot/generate_pages.xml")
     system_msg = SystemMessage(content=instructions)
 
-    msg = state["messages"]
+    msg = state["messages"][-1]
 
-    print(msg)
+    logger.info("--- Generate Pages --- ")
+    logger.info(msg.content)
 
     user_msg = HumanMessage(content=msg.content)
+
+    
     structured_llm = model.with_structured_output(
         PagesClass,
         method="json_mode",
@@ -41,6 +51,7 @@ def generate_pages(state: MessagesState):
     conversation = [system_msg] + [user_msg]
     output = structured_llm.invoke(conversation, stream=False, response_format="json")
 
-    print(output['parsed']['pages'])
+    # print(output['parsed']['pages'])
 
     return { "pages": output['parsed']['pages'] }
+    return None
