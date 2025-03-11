0e2cd4f16af685e80bd351ac489360e6bd522de8
Wouter Born <wouter@born.gs>
2025-03-11 16:02:08 +0100

    Undo the chatgpt change

--- Classes/llm_classes.py
+++ Classes/llm_classes.py
@@ -1,14 +1,13 @@
 from langchain_groq import ChatGroq
-from langchain.chat_models import ChatOpenAI
 ##################################
 # LLM Setup
 ##################################
-# modelVers = ChatGroq(
-#     temperature=0,
-#     model_name="llama-3.3-70b-versatile",
-#     api_key="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",
-#     disable_streaming=True
-# )
+modelVers = ChatGroq(
+    temperature=0,
+    model_name="llama-3.3-70b-versatile",
+    api_key="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",
+    disable_streaming=True
+)
 
 modelDeepSpec = ChatGroq(
     temperature=1,
@@ -18,17 +17,17 @@ modelDeepSpec = ChatGroq(
 )
 
 
-# modelSpec = ChatGroq(
+modelSpec = ChatGroq(
+    temperature=0,
+    model_name="llama-3.3-70b-versatile",
+    api_key="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",
+    disable_streaming=True
+)
+
+# modelVers = ChatOpenAi(
 #     temperature=0,
-#     model_name="llama-3.3-70b-versatile",
-#     api_key="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",
-#     disable_streaming=True
-#)
+#     model_name="ChatGPT-4o")
 
-modelVers = ChatOpenAI(
-    temperature=0,
-    model_name="gpt-4o-mini")
-
-modelSpec = ChatOpenAI(
-    temperature=0,
-    model_name="gpt-4o-mini")
+# modelSpec = ChatOpenAi(
+#     temperature=0,
+#     model_name="ChatGPT-4o")
